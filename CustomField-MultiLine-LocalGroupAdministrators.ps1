d209ac92532be8c2f965c97d751896c88b282120
kjentech <84007507+kjentech@users.noreply.github.com>
2024-06-21 06:49:46 +0200

    Create CustomField-MultiLine-LocalGroupAdministrators.ps1

---
+++ CustomField-MultiLine-LocalGroupAdministrators.ps1
@@ -0,0 +1,8 @@
+$Domain = (Get-CimInstance Win32_ComputerSystem).Domain
+$DomainNetBIOS = ($Domain -split "\.")[0]
+$ExcludeMembers = "$Domain\Domain Admins","$DomainNetBIOS\Domain Admins",$env:computername\Administrator"
+$LocalGroup = Get-LocalGroupMember -SID S-1-5-32-544 | where Name -notin $ExcludeMembers
+
+$LocalGroupOutput = $LocalGroupAdministrators.Name | Out-String
+
+Ninja-Property-Set localGroupAdministrators $LocalGroupOutput
