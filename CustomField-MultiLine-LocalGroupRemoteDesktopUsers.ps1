da8663f4ebc2fcbb77ddcc50b0dca9434e425bca
kjentech <84007507+kjentech@users.noreply.github.com>
2024-06-21 12:55:22 +0200

    Update CustomField-MultiLine-LocalGroupRemoteDesktopUsers.ps1

--- CustomField-MultiLine-LocalGroupRemoteDesktopUsers.ps1
+++ CustomField-MultiLine-LocalGroupRemoteDesktopUsers.ps1
@@ -4,6 +4,7 @@ $ExcludeMembers = "$Domain\Domain Admins","$DomainNetBIOS\Domain Admins"
 $LocalGroup = Get-LocalGroupMember -SID S-1-5-32-555 | where Name -notin $ExcludeMembers
 
 $LocalGroupOutput = $LocalGroup.Name | Out-String
+$LocalGroupOutput
 
 if ([string]::IsNullOrEmpty($LocalGroupOutput)) {
   Ninja-Property-Clear localGroupRdpUsers
