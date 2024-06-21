68a3df4555f4341340ff1253aa5c53f89d729618
kjentech <84007507+kjentech@users.noreply.github.com>
2024-06-21 11:12:41 +0200

    Update CustomField-MultiLine-LocalGroupRemoteDesktopUsers.ps1

--- CustomField-MultiLine-LocalGroupRemoteDesktopUsers.ps1
+++ CustomField-MultiLine-LocalGroupRemoteDesktopUsers.ps1
@@ -1,6 +1,6 @@
 $Domain = (Get-CimInstance Win32_ComputerSystem).Domain
 $DomainNetBIOS = ($Domain -split "\.")[0]
-$ExcludeMembers = "$Domain\Domain Users","$DomainNetBIOS\Domain Users"
+$ExcludeMembers = "$Domain\Domain Admins","$DomainNetBIOS\Domain Admins"
 $LocalGroup = Get-LocalGroupMember -SID S-1-5-32-555 | where Name -notin $ExcludeMembers
 
 $LocalGroupOutput = $LocalGroup.Name | Out-String
